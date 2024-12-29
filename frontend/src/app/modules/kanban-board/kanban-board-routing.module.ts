7f83ad3c32d62a38bae129f44cd2dfab9c9d9e0f
Jakub Puchala <86433812+ImJakubPuchala@users.noreply.github.com>
2024-12-29 12:05:49 +0100

    Kanban Board - v1

--- frontend/src/app/modules/kanban-board/kanban-board-routing.module.ts
+++ frontend/src/app/modules/kanban-board/kanban-board-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { KanbanBoardHomeComponent } from './pages/kanban-board-home/kanban-board-home.component';
+import { KanbanBoardMainComponent } from './pages/kanban-board-main/kanban-board-main.component';
 
 const routes: Routes = [
   {
     path: '',
     component: KanbanBoardHomeComponent,
+  },
+  {
+    path: 'project/:name',
+    component: KanbanBoardMainComponent,
   },
 ];
 
--- frontend/src/app/modules/kanban-board/kanban-board.module.ts
+++ frontend/src/app/modules/kanban-board/kanban-board.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { KanbanBoardRoutingModule } from './kanban-board-routing.module';
 import { KanbanBoardHomeComponent } from './pages/kanban-board-home/kanban-board-home.component';
-
+import { KanbanBoardMainComponent } from './pages/kanban-board-main/kanban-board-main.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    KanbanBoardHomeComponent
-  ],
+  declarations: [KanbanBoardHomeComponent, KanbanBoardMainComponent],
   imports: [
     CommonModule,
-    KanbanBoardRoutingModule
-  ]
+    KanbanBoardRoutingModule,
+    DragDropModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
 })
-export class KanbanBoardModule { }
+export class KanbanBoardModule {}
